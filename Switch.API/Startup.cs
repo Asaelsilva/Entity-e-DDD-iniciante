c5ac41cae312744aa82dfdcb2017552e303bef86
Asael <asael.guitar_g3@hotmail.com>
2019-11-20 23:31:46 -0300

    Adicionar arquivos de projeto.

---
+++ Switch.API/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Switch.Infra.Data.Context;
+
+namespace Switch.API
+{
+    public class Startup
+    {
+        IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("config.json");
+            Configuration = builder.Build();
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var conn = Configuration.GetConnectionString("SwitchDB");
+            services.AddDbContext<SwitchContext>(options => options.UseLazyLoadingProxies()
+                                                  .UseSqlServer(conn, m => m.MigrationsAssembly("Switch.Infra.Data")));
+            services.AddMvcCore();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
