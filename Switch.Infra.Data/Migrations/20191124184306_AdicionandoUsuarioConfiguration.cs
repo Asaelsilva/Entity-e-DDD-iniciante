225949911afc368ec3b37a68115caaed98778eb5
Asael <asael.guitar_g3@hotmail.com>
2019-11-24 16:03:24 -0300

    Fluent API e Mapeando entidade por configuração

---
+++ Switch.Infra.Data/Migrations/20191124184306_AdicionandoUsuarioConfiguration.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Switch.Infra.Data.Migrations
+{
+    public partial class AdicionandoUsuarioConfiguration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UrlFoto",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SobreNome",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Senha",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Usuarios",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UrlFoto",
+                table: "Usuarios",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SobreNome",
+                table: "Usuarios",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Senha",
+                table: "Usuarios",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Usuarios",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Usuarios",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Usuarios",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
